d78251c8a03c64e8bb3743c5d3505bc52c9a3caa
hannahker <hannah.ker96@gmail.com>
2025-05-27 13:25:54 -0700

    remove unneeded files

--- src/config/settings.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-AZURE_DB_PW_DEV = os.getenv("AZURE_DB_PW_DEV")
-AZURE_DB_UID = os.getenv("AZURE_DB_UID")
--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,13 +1,11 @@
 from .storms import Storm
 from .observed_track import ObservedTrack
 from .forecast_track import ForecastTrack
-from .forecast_track_ensemble import ForecastTrackEnsemble
 from .database import init_db
 
 __all__ = [
     "Storm",
     "ObservedTrack",
     "ForecastTrack",
-    "ForecastTrackEnsemble",
     "init_db",
 ]
