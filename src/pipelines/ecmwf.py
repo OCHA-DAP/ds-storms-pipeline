1db9c3ce801418bd31511a6232249867829fbdff
isatotun <isabelle.tot@un.org>
2025-10-28 10:58:35 -0300

    Adding save_dir

--- src/pipelines/ecmwf.py
+++ src/pipelines/ecmwf.py
@@ -37,6 +37,7 @@ def retrieve_ecmwf(
             start_date=start_date,
             end_date=end_date,
             stage=stage,
+            temp_dir=save_dir
         )
         df.to_csv(file_path, index=False, na_rep=None)
         logger.info(f"Successfully wrote ECMWF data to {file_path}.")
