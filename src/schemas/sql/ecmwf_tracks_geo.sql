fee6e0791e503b1862ac10225aec3b307a1ae426
hannahker <hannah.ker96@gmail.com>
2025-12-04 11:49:43 -0800

    adjust unique constraint and add number column

--- src/schemas/sql/ecmwf_tracks_geo.sql
+++ src/schemas/sql/ecmwf_tracks_geo.sql
@@ -1,12 +1,13 @@
 -- Table: storms.ecmwf_tracks_geo
 
--- DROP TABLE IF EXISTS storms.ecmwf_tracks_geo;
+DROP TABLE IF EXISTS storms.ecmwf_tracks_geo_hannah;
 
 CREATE TABLE IF NOT EXISTS storms.ecmwf_tracks_geo_hannah(
     issued_time timestamp without time zone,
     provider VARCHAR,
     forecast_id VARCHAR,
     basin VARCHAR,
+    number VARCHAR,
     leadtime bigint,
     valid_time timestamp without time zone,
     pressure double precision,
@@ -14,7 +15,7 @@ CREATE TABLE IF NOT EXISTS storms.ecmwf_tracks_geo_hannah(
     storm_id VARCHAR,
     point_id VARCHAR,
     geometry geometry(Point,4326),
-    CONSTRAINT ecmwf_tracks_geo_hannah_unique UNIQUE (storm_id, valid_time, issued_time),
+    CONSTRAINT ecmwf_tracks_geo_hannah_unique UNIQUE (forecast_id, valid_time, issued_time, geometry, wind_speed, pressure, leadtime, basin, number),
 	CONSTRAINT foreign_key_sid FOREIGN KEY (storm_id)
 	REFERENCES storms.ecmwf_storms_hannah(storm_id)
 );
