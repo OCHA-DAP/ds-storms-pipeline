c69b20273c7ea250496abc8e3b3f187339357dc0
hannahker <hannah.ker96@gmail.com>
2025-12-15 15:40:59 -0800

    update input arguments

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,83 +1,82 @@
 import argparse
-import sys
+from datetime import datetime
 
 from src.pipelines.ecmwf import run_ecmwf
 from src.pipelines.ibtracs import run_ibtracs
 
 
 def main():
-    main_parser = argparse.ArgumentParser()
-    main_parser.add_argument(
-        "pipeline",
-        choices=["ibtracs", "ecmwf"],
-        help="Pipeline to run",
-    )
-    main_parser.add_argument(
+    parser = argparse.ArgumentParser(description="Run storm data pipelines")
+    subparsers = parser.add_subparsers(dest="pipeline", required=True)
+
+    # Common arguments as a parent parser
+    common = argparse.ArgumentParser(add_help=False)
+    common.add_argument(
         "--mode",
         choices=["dev", "prod"],
         default="dev",
         help="Mode to run the pipeline in",
     )
-    main_parser.add_argument(
+    common.add_argument(
+        "--chunksize",
+        type=int,
+        default=10000,
+        help="Chunksize to use in SQL operations",
+    )
+
+    # IBTrACS subparser
+    ibtracs_parser = subparsers.add_parser(
+        "ibtracs", parents=[common], help="Run IBTrACS pipeline"
+    )
+    ibtracs_parser.add_argument(
         "--save-to-blob",
-        choices=[True, False],
-        default=False,
-        nargs="?",
+        action="store_true",
         help="Save netcdf file in blob",
     )
-    main_parser.add_argument(
+    ibtracs_parser.add_argument(
         "--dataset-type",
         choices=["last3years", "ACTIVE", "ALL"],
         default="last3years",
-        nargs="?",
         help="Which dataset type to use",
     )
-    main_parser.add_argument(
-        "--start-date",
-        default="2019-01-01",
-        nargs="?",
-        help="Which date to start",
-    )
-    main_parser.add_argument(
-        "--end-date",
-        default="2019-12-31",
-        nargs="?",
-        help="Which date to end",
-    )
-    main_parser.add_argument(
+    ibtracs_parser.add_argument(
         "--save-dir",
         default="/tmp",
-        nargs="?",
         help="Where to save downloaded file",
     )
-    main_parser.add_argument(
-        "--chunksize",
-        default=10000,
-        nargs="?",
-        help="Which chunksize to use in sql",
+
+    # ECMWF subparser
+    ecmwf_parser = subparsers.add_parser(
+        "ecmwf", parents=[common], help="Run ECMWF pipeline"
+    )
+    ecmwf_parser.add_argument(
+        "--start-date",
+        default="2019-01-01",
+        help="Start date (YYYY-MM-DD)",
+    )
+    ecmwf_parser.add_argument(
+        "--end-date",
+        default="2019-12-31",
+        help="End date (YYYY-MM-DD)",
     )
 
-    args, remaining_args = main_parser.parse_known_args()
-    sys.argv = [sys.argv[0]] + remaining_args
+    args = parser.parse_args()
 
     if args.pipeline == "ibtracs":
         run_ibtracs(
-            args.mode,
-            args.save_to_blob,
-            args.save_dir,
-            args.chunksize,
+            mode=args.mode,
+            dataset_type=args.dataset_type,
+            save_to_blob=args.save_to_blob,
+            save_dir=args.save_dir,
+            chunksize=args.chunksize,
         )
     elif args.pipeline == "ecmwf":
-        from datetime import datetime
-
         run_ecmwf(
             mode=args.mode,
             start_date=datetime.strptime(args.start_date, "%Y-%m-%d"),
             end_date=datetime.strptime(args.end_date, "%Y-%m-%d"),
             chunksize=args.chunksize,
         )
-    else:
-        raise ValueError(f"Unknown pipeline: {args.pipeline}")
 
 
 if __name__ == "__main__":
