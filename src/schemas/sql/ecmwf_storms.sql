c49b98696ef80cc33fcb35582fe7a20fef85e847
hannahker <hannah.ker96@gmail.com>
2025-12-16 10:29:05 -0800

    remove testing table from sql

--- src/schemas/sql/ecmwf_storms.sql
+++ src/schemas/sql/ecmwf_storms.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS storms.ecmwf_storms_hannah(
+CREATE TABLE IF NOT EXISTS storms.ecmwf_storms(
     number VARCHAR NOT NULL,
     provider VARCHAR NOT NULL,
     season BIGINT CHECK (season BETWEEN 2005 AND 2050) NOT NULL,
     genesis_basin VARCHAR NOT NULL,
     name VARCHAR NULL,
     storm_id VARCHAR,
-    CONSTRAINT ecmwf_storms_hannah_unique PRIMARY KEY (storm_id)
+    CONSTRAINT ecmwf_storms_unique PRIMARY KEY (storm_id)
 );
 
 TABLESPACE pg_default;
--- src/schemas/sql/ecmwf_tracks_geo.sql
+++ src/schemas/sql/ecmwf_tracks_geo.sql
@@ -1,8 +1,8 @@
 -- Table: storms.ecmwf_tracks_geo
 
-DROP TABLE IF EXISTS storms.ecmwf_tracks_geo_hannah;
+DROP TABLE IF EXISTS storms.ecmwf_tracks_geo;
 
-CREATE TABLE IF NOT EXISTS storms.ecmwf_tracks_geo_hannah(
+CREATE TABLE IF NOT EXISTS storms.ecmwf_tracks_geo(
     issued_time timestamp without time zone,
     provider VARCHAR,
     forecast_id VARCHAR,
@@ -15,15 +15,15 @@ CREATE TABLE IF NOT EXISTS storms.ecmwf_tracks_geo_hannah(
     storm_id VARCHAR,
     point_id VARCHAR,
     geometry geometry(Point,4326),
-    CONSTRAINT ecmwf_tracks_geo_hannah_unique UNIQUE (forecast_id, valid_time, issued_time, geometry, wind_speed, pressure, leadtime, basin, number),
+    CONSTRAINT ecmwf_tracks_geo_unique UNIQUE (forecast_id, valid_time, issued_time, geometry, wind_speed, pressure, leadtime, basin, number),
 	CONSTRAINT foreign_key_sid FOREIGN KEY (storm_id)
-	REFERENCES storms.ecmwf_storms_hannah(storm_id)
+	REFERENCES storms.ecmwf_storms(storm_id)
 );
 
-ALTER TABLE IF EXISTS storms.ecmwf_tracks_geo_hannah
+ALTER TABLE IF EXISTS storms.ecmwf_tracks_geo
     OWNER to {owner};
 
-CREATE INDEX IF NOT EXISTS idx_ecmwf_tracks_geo_geometry_hannah
-    ON storms.ecmwf_tracks_geo_hannah USING gist
+CREATE INDEX IF NOT EXISTS idx_ecmwf_tracks_geo_geometry
+    ON storms.ecmwf_tracks_geo USING gist
     (geometry)
     TABLESPACE pg_default;
